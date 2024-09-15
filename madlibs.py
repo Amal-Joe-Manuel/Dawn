f3f03141b05ddd070d07f09300bd64b1cacb36d8
Amal-Joe-Manuel <amaljoemanuel13@gmail.com>
2024-09-15 22:59:47 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+colour = input("Enter a colour: ") 
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter any celebrity: ")
+
+print("I like " +colour +" clothes" )
+print(plural_noun+" are available")
+print(celebrity+" is my favourite")
