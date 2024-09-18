6b82eae7241574c1b086a1c614d86a370e9a875f
Amal-Joe-Manuel <amaljoemanuel13@gmail.com>
2024-09-18 23:07:11 +0530

    experiment with ifs

---
+++ prank.py
@@ -0,0 +1,12 @@
+is_girl= True
+in_love = True
+
+if is_girl and in_love:
+    print ("Hello Stranger <3")
+elif is_girl and not(in_love):
+    print("Hello not my stranger :/")
+elif not(is_girl) and in_love:
+    print("why are you gay")        
+else: 
+    print("Go die")
+
